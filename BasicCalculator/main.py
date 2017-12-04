f6c0b5931d5a30445638345488529b650d3d6724
Duranskinho <duranski17@gmail.com>
2017-12-04 19:35:30 +1300

    added basic calculator project

---
+++ BasicCalculator/main.py
@@ -0,0 +1,11 @@
+import math
+
+if __name__ == '__main__':
+
+    num1 = int(input("enter a number: "))
+
+    num2 = int(input("enter another number: "))
+
+    sum = num1 + num2
+
+    print("the sum of the two numbers is: " + str(sum))
